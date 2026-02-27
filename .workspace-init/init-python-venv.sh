d23240ff0fcf61b34f08092fbe8d46a5bf260db1
cgruver <cgruver@redhat.com>
2026-02-27 19:37:07 +0000

    refactor ready for test
    Signed-off-by: cgruver <cgruver@redhat.com>

---
+++ .workspace-init/init-python-venv.sh
@@ -0,0 +1,7 @@
+if [[ ! -d ${PROJECTS_ROOT}/.venv ]]
+then
+  python -m venv --copies ${PROJECTS_ROOT}/.venv
+  source ${PROJECTS_ROOT}/.venv/bin/activate
+  pip install --upgrade pip
+  pip install -r ${PROJECTS_SOURCE}/.workspace-init/requirements.txt
+fi
